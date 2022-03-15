a32d415e998a50deeb93c096c508d55d7d28f011
ReitoSerizawa <s.reitiger@gmail.com>
2022-03-14 20:07:42 -0400

    commit tree 27056d3f6e2e243410f6580b0db95997bdf51f1e

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(){
+}
+
+function subtract(){
+}
+
+function multiply(){
+}
+
+function divide(){
+}
+
+function add(a = 1000, b = 180){
+    return a + b
+}
+
+function subtract(a=0, b=442){
+    return a - b
+}
+
+function multiply(a = 172473, b = 2){
+return a * b
+}
+
+function divide(a = 4911894273127753, b = 0.000000000000001){
+return a/b
+}
+
+function increment(a = 414){
+return a += 1
+}
+
+function decrement(a = 500){
+    return a-= 1
+}
+
+function makeInt(string){
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string, 10)
+}
+
